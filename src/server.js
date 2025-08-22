b97660d7c7983e5567bcf4a358ef870440f4d241
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-08-22 12:41:03 +0200

    Add server.js and setup basic Express server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/routes.js';
+
+const app = express();
+app.use(express.json());
+
+
+app.use('/api', routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
