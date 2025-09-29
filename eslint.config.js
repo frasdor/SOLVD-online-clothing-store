5dce2420dae846addddc6caef5b1e4b23a2ade3f
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-09-29 15:14:41 +0200

    Add working ESLint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,21 @@
-export default [
+module.exports = [
   {
     files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+      },
+    },
     rules: {
       "no-unused-vars": "warn",
-      "no-console": "off"
+      "no-console": "off",
     },
   },
 ];
